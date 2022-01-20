8b061c02274d2f604a8ecddb9acbc99232662539
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 11:04:07 +0700

    Create ex8.sql

---
+++ ex8.sql
@@ -0,0 +1,15 @@
+WITH list_repo AS (
+SELECT repo_name 
+FROM `bigquery-public-data.github_repos.languages`, UNNEST (language) AS language
+WHERE language.name = 'Java'
+)
+SELECT author.name AS name,
+COUNT (commit) AS count
+FROM `bigquery-public-data.github_repos.sample_commits`AS samples
+-- , UNNEST (committer) AS committer
+INNER JOIN list_repo ON list_repo.repo_name = samples.repo_name
+WHERE committer.date >= TIMESTAMP("2016-01-01 00:00:00 UTC")
+AND committer.date <= TIMESTAMP("2017-01-01 00:00:00 UTC")
+GROUP BY name
+ORDER BY count DESC
+LIMIT 10
