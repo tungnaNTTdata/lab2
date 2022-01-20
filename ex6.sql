903470e8c5b753e06afcb0b05f8bab999f1961ae
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 15:42:23 +0700

    Update ex6.sql

--- ex6.sql
+++ ex6.sql
@@ -1,13 +1,14 @@
 WITH list_answer_on_2010 AS (
 SELECT accepted_answer_id FROM `bigquery-public-data.stackoverflow.stackoverflow_posts`
 WHERE creation_date >= TIMESTAMP("2010-01-01 00:00:00 UTC")
-  AND creation_date <= TIMESTAMP("2011-01-01 00:00:00 UTC")
-LIMIT 1000)
+  AND creation_date < TIMESTAMP("2011-01-01 00:00:00 UTC")
+)
 SELECT owner_user_id AS id_user,
 COUNT (id) AS COUNT
 FROM `list_answer_on_2010`
 INNER JOIN `bigquery-public-data.stackoverflow.posts_answers`
 ON list_answer_on_2010.accepted_answer_id = id
+WHERE owner_user_id IS NOT NULL
 GROUP BY owner_user_id
 ORDER BY count DESC
 LIMIT 10
