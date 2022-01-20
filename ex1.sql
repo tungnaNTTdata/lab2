bac9e6957c133b0a22e4ef4aa2ba2a17df757ae5
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 15:12:46 +0700

    Update ex1.sql

--- ex1.sql
+++ ex1.sql
@@ -1,6 +1,9 @@
+WITH raw_table AS (
 SELECT EXTRACT(YEAR FROM Release_Date) AS year , Movie_Title, Production_Budget,
-RANK() OVER(PARTITION BY Release_Date ORDER BY Production_Budget ASC) AS rank
+RANK() OVER(PARTITION BY (EXTRACT(YEAR FROM Release_Date)) ORDER BY Production_Budget DESC) AS rank
 FROM `nttdata-c4e-bde.uc1_10.Movie`
-WHERE Release_Date >= DATE("2016-01-01") AND Release_Date <= DATE("2020-12-31")
-ORDER BY year DESC 
-LIMIT 10
+)
+SELECT year, Movie_Title, Production_Budget, rank
+FROM raw_table
+WHERE raw_table.year  >= 2016 AND raw_table.year  <= 2020 AND rank <= 10
+ORDER BY year DESC , rank
