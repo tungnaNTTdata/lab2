6e540366f8af7b0361777a06020eafa49b499b54
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 15:23:47 +0700

    Update ex3.sql

--- ex3.sql
+++ ex3.sql
@@ -1,6 +1,5 @@
 SELECT date, h.page.pagePath as pagePath,
 COUNT (h.page.pagePath) as counter
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170701`, UNNEST (hits) as h 
-WHERE date = '20170701'
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017070*`, UNNEST (hits) as h 
 GROUP BY pagePath, date
 LIMIT 1000
