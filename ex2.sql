257cf9ac6786c1a527fd5a53f224820f8e87caaa
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 15:25:13 +0700

    Update ex2.sql

--- ex2.sql
+++ ex2.sql
@@ -1,4 +1,3 @@
 SELECT visitId, visitStartTime, h.page.pageTitle as pageTitle, h.page.pagePath as pagePath 
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`, UNNEST(hits) as h
-WHERE date = '20170801' 
 LIMIT 1000
