547fd27817c7efaf1711a66acd2c3b81e4e60a00
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 11:50:14 +0700

    Update ex9.sql

--- ex9.sql
+++ ex9.sql
@@ -1,8 +1,8 @@
 SELECT repo_name, difference.new_path AS file, committer.date AS date, 
 LAG(commit)
-OVER (PARTITION BY difference.new_path ORDER BY committer.date ASC) AS previous_repo,
+OVER (PARTITION BY difference.new_path ORDER BY committer.date ASC) AS previous_commit,
 commit,
 LEAD(commit)
-OVER(PARTITION BY difference.new_path ORDER BY committer.date ASC) AS after_repo
+OVER(PARTITION BY difference.new_path ORDER BY committer.date ASC) AS next_commit
 FROM `bigquery-public-data.github_repos.sample_commits`, UNNEST (difference) AS difference
 WHERE difference.new_path LIKE '%kernel/acct.c%'
