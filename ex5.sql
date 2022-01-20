1a9e485957442db4d182980150a87b95fe71e0be
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 15:39:15 +0700

    Update ex5.sql

--- ex5.sql
+++ ex5.sql
@@ -1,6 +1,7 @@
 SELECT owner_user_id AS id_user, 
 COUNT (id) AS count
 FROM `bigquery-public-data.stackoverflow.posts_answers`
+WHERE owner_user_id IS NOT NULL
 GROUP BY owner_user_id
 ORDER BY count DESC
 LIMIT 10
