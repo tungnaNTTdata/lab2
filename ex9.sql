021c26313dd11d7d85d8b9172b36234d92b409e9
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-20 16:02:39 +0700

    Update ex9.sql

--- ex9.sql
+++ ex9.sql
@@ -5,4 +5,4 @@ commit,
 LEAD(commit)
 OVER(PARTITION BY difference.new_path ORDER BY committer.date ASC) AS next_commit
 FROM `bigquery-public-data.github_repos.sample_commits`, UNNEST (difference) AS difference
-WHERE difference.new_path LIKE '%kernel/acct.c%'
+WHERE difference.new_path LIKE 'kernel/%.c' AND repo_name LIKE '%linux'
