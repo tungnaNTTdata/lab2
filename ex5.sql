2ab1c5ade09f42be378b2fcf937d46bf6bbe24d7
tungnaNTTdata <89640551+tungnaNTTdata@users.noreply.github.com>
2022-01-21 14:44:09 +0700

    Update ex5.sql

--- ex5.sql
+++ ex5.sql
@@ -1,7 +1,9 @@
 SELECT owner_user_id AS id_user, 
 COUNT (id) AS count
 FROM `bigquery-public-data.stackoverflow.posts_answers`
-WHERE owner_user_id IS NOT NULL
+WHERE owner_user_id IS NOT NULL 
+  AND creation_date >= TIMESTAMP("2010-01-01 00:00:00 UTC")
+  AND creation_date < TIMESTAMP("2011-01-01 00:00:00 UTC")
 GROUP BY owner_user_id
 ORDER BY count DESC
 LIMIT 10
